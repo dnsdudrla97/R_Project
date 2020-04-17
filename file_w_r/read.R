2bc071a554ff1d74ea99dad07b674d9a3c912503
dnsdudrla97 <dnsdudrla97@naver.com>
2020-04-17 09:21:54 +0900

    0417_R_write

--- file_w_r/read.R
+++ file_w_r/read.R
@@ -1,5 +1,7 @@
 # file data read (.csv, .xlsx) #workingdir
-setwd("C:/Users/Younsle/Desktop/R/file_w_r/data")       # 작업 폴더 지정
-read_test<-read.csv("airquality.csv", header=T)         # .csv 파일 읽기 header=T -> 데이터 첫 번째 줄 열의 이름으로 출력 (header = F ->값으로만)
+# setwd("C:/Users/Younsle/Desktop/R/file_w_r/data")       # 작업 폴더 지정
+# read_test<-read.csv("airquality.csv", header=T)         # .csv 파일 읽기 header=T -> 데이터 첫 번째 줄 열의 이름으로 출력 (header = F ->값으로만)
+read_test<-read.csv(file.choose(), header=T)         # 파일의 경로를 모를경우 file.choose() 사용하여 파일 탐색기를 작동한다.
 head(read_test)
 str(read_test)
+
--- file_w_r/write.R
+++ file_w_r/write.R
@@ -1,1 +1,6 @@
-#write()
+setwd("C:/Users/Younsle/Desktop/R/file_w_r/data")       # 작업 폴더 지정
+my.iris<-subset(iris, Species='Setosa')                 # Setoas 품종 데이터 추출
+write.csv(my.iris, "my_iris.csv", row.names=F)          # row.names=F -> 행 번호를 붙이지 않겠다.
+
+
+
